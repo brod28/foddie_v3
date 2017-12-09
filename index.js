04caff4309b4866304fd5ccf1f22c4f7ed10ecda
DimaBenivo <dima@benivo.com>
2017-12-09 14:45:34 +0000

    automatic push

--- index.js
+++ index.js
@@ -84,6 +84,7 @@ restService.use(express.static(path.join(__dirname, 'client/build')));
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 restService.get('*', (req, res) => {
+    console.log("request index");
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
